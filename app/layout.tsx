b8dea604d1e451c33968f92fcfd9afab4715b79d
ursus161 <stefanghetu9@gmail.com>
2023-09-14 15:33:12 +0300

    progres main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html className='all' lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
