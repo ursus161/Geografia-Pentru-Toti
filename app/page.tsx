788bb9c6178e815d2ab4453b6982c516256ef21d
Stefan Ghetu <106436265+ursus161@users.noreply.github.com>
2023-11-16 19:20:06 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <h1 className="titlu">Geografia Distractiva</h1>
         </div>
      
-    <div className="container relative bottom-12 left-36 ">
+    <div className="container relative bottom-4 left-36 ">
         <div className="grid">
         
           <Link href="https://prezi.com/view/Qp0IGCvwb2Y5pFsRXGgI/">
