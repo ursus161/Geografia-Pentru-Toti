5061ac6908337511449f174882e887997af5a5f8
ursus161 <stefanghetu9@gmail.com>
2023-11-24 17:32:58 +0200

    mutat titlul

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
     <div className="relative bottom-20 ">
-        <Image className="relative top-48 left-20 pt-0 mt-0" src={carte} alt="Carte" width={100} height={90}/>
+        <Image className="relative top-48 left-16 pt-0 mt-0" src={carte} alt="Carte" width={100} height={90}/>
         <h1 className="titlu">GeoSmart</h1>
       </div>
      
