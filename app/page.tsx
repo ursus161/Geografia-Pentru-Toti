ffa838e64f02efc4defeaa3cb86ddb49f61d3e26
ursus161 <stefanghetu9@gmail.com>
2023-11-29 19:21:10 +0200

    small changes on the grid placement

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ import Grid from "./grid";
 export default function Home() {
   
   return (
-    <>
-    <div className="relative bottom-20 ">
+    <div className="mb-28">
+    <div className="relative bottom-20 left-6 mb-6">
         <Image className="relative top-48 left-16 pt-0 mt-0" src={carte} alt="Carte" width={100} height={90}/>
-        <h1 className="titlu relative right-2">GeoSmart</h1>
+        <h1 className="titlu relative right-2 ">GeoSmart</h1>
       </div>
      
-     <div className="flex justify-center items-center relative top-40">
+     <div className="flex justify-center items-center  relative top-24 ">
      
       <Grid/>
     </div>
-    </>
+    </div>
   )
 }
