b69cb824b002bf45d604ffa44b807394099eeb81
ursus161 <stefanghetu9@gmail.com>
2023-11-29 20:15:38 +0200

    titlu

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   
   return (
     <div className="mb-28">
-    <div className="relative bottom-20 left-6 mb-6">
+    <div className="relative bottom-20 left-2 mb-6">
         <Image className="relative top-48 left-16 pt-0 mt-0" src={carte} alt="Carte" width={100} height={90}/>
         <h1 className="titlu relative right-2 ">GeoSmart</h1>
       </div>
